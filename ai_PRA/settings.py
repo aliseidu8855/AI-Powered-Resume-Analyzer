47acd463f893a60eea14764bfaefa16b9fe35370
aliseidu8855 <aliseidu8855@gmail.com>
2025-03-17 12:11:52 +0000

    Add new apps: resume, users, and analysis to settings
    Included `resume`, `users`, and `analysis` in the installed apps list to enable their functionality. This change prepares the project for new features related to these apps.

--- ai_PRA/settings.py
+++ ai_PRA/settings.py
@@ -40,7 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
+    'resume',
+    'users',
+    'analysis'
 ]
 
 MIDDLEWARE = [
